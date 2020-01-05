63ba6bd8d6367e10e17d78bf8d0e34a3c322c66e
github username <Joemiller065@gmail.com>
2020-01-05 07:52:17 -0500

    basic structure completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
+import { Grid } from '@material-ui/core';
+import youtube from './api/youtube';
 
 class App extends React.Component {
     render(){
         return(
-            <h1>Youtube Clone</h1>
+            <Grid justify="center" container spacing={16}>
+                <Grid item xs={12}>
+                    <Grid container spacing={16}>
+                        <Grid item xs={12}>
+                            {/* SEARCH BAR */}
+                        </Grid>
+                        <Grid item xs={8}>
+                            {/* VIDEO DETAIL */}
+                        </Grid>
+                        <Grid item xs={4}>
+                            {/* VIDEO LIST */}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         )
     }
 }
